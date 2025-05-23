4c39986ffd65fc8228e8b33e71d59a7af5682332
ronaldG <ronaldalejandrogallocastellano@gmail.com>
2025-05-23 08:37:33 -0500

    Ajueste nombre base de datos

--- data.sql
+++ data.sql
@@ -1,11 +1,17 @@
 # Creacion usuarios
-create user 'dylan_dom_api'@'localhost' identified by '1100954165';
+create user 'dylan_ronald_dom_api'@'localhost' identified by '1100954165';
 # creacion de la base de datos
 create database eventos_dom_api;
 
-grant all on eventos_dom_api.* to dylan_dom_api@localhost;
+grant all on eventos_dom_api.* to dylan_ronald_dom_api@localhost;
 
 flush privileges;
+
+PORT=3000
+DB_HOST=localhost
+DB_USER=dylan_ronald_dom_api
+DB_PASSWORD=1100954165
+DB_NAME=eventos_dom_api
 
 create table ciudades (
 id_ciudad int auto_increment,
@@ -68,7 +74,20 @@ insert into generos(genero) values
 ('Femenino'),
 ('Otros');
 
-select * from usuarios;
-drop table ciudades;
 insert into usuarios (nombre, apellido, telefono, documento, usuario, contrasena, id_ciudad, id_genero) values 
-('Dylan', 'Bravo', 3204019091, 123456789, 'Dybravo27', '150195', 1, 1);
+('Dylan', 'Bravo', 3002040190, 123456789, 'Dybravo27', '150195', 1, 1),
+('Camila', 'Ríos', 3123456789, 111111111, 'CamRios', 'cami2025', 2, 2),
+('Mateo', 'López', 3201234567, 222222222, 'MateoL', 'mateo2025', 4, 1),
+('Valentina', 'Suárez', 3009876543, 333333333, 'ValeS', 'valentina25', 3, 2),
+('Samuel', 'Castro', 3112233445, 444444444, 'SamCastro', 'samuel25', 5, 1);
+
+insert into lenguaje_usuario (id_usuario, id_lenguaje) values 
+(1, 1),
+(2, 1),
+(3, 4), 
+(4, 2),
+(4, 3),
+(5, 6);
+
+select * from lenguaje_usuario;
+drop table ciudades;
